3cda8b85388785e57b7f5566fdb6d70650e7ea63
superbabii <nzrbabii@gmail.com>
2024-10-22 11:29:12 +0300

    initial

---
+++ src/medrag.py
@@ -0,0 +1,274 @@
+import os
+import re
+import json
+import torch
+import transformers
+from transformers import AutoTokenizer
+import sys
+sys.path.append("src")
+from utils import RetrievalSystem, DocExtracter
+from template import *
+
+class MedRAG:
+
+    def __init__(self, llm_name="OpenAI/gpt-3.5-turbo-16k", rag=True, follow_up=False, retriever_name="MedCPT", corpus_name="Textbooks", db_dir="./corpus", cache_dir=None, corpus_cache=False, HNSW=False):
+        self.llm_name = llm_name
+        self.rag = rag
+        self.retriever_name = retriever_name
+        self.corpus_name = corpus_name
+        self.db_dir = db_dir
+        self.cache_dir = cache_dir
+        self.docExt = None
+        if rag:
+            self.retrieval_system = RetrievalSystem(self.retriever_name, self.corpus_name, self.db_dir, cache=corpus_cache, HNSW=HNSW)
+        else:
+            self.retrieval_system = None
+        self.templates = {"cot_system": general_cot_system, "cot_prompt": general_cot,
+                    "medrag_system": general_medrag_system, "medrag_prompt": general_medrag}
+        
+        self.max_length = 2048
+        self.context_length = 1024
+        self.tokenizer = AutoTokenizer.from_pretrained(self.llm_name, cache_dir=self.cache_dir)
+
+        self.tokenizer.chat_template = open('./templates/pmc_llama.jinja').read().replace('    ', '').replace('\n', '')
+
+        self.model = transformers.pipeline(
+            "text-generation",
+            model=self.llm_name,
+            # torch_dtype=torch.float16,
+            torch_dtype=torch.bfloat16,
+            device_map="auto",
+            model_kwargs={"cache_dir":self.cache_dir},
+        )
+        
+        self.follow_up = follow_up
+        if self.rag and self.follow_up:
+            self.answer = self.i_medrag_answer
+            self.templates["medrag_system"] = simple_medrag_system
+            self.templates["medrag_prompt"] = simple_medrag_prompt
+            self.templates["i_medrag_system"] = i_medrag_system
+            self.templates["follow_up_ask"] = follow_up_instruction_ask
+            self.templates["follow_up_answer"] = follow_up_instruction_answer
+        else:
+            self.answer = self.medrag_answer
+
+    def generate(self, messages):
+
+        stopping_criteria = None
+        prompt = self.tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
+
+        response = self.model(
+            prompt,
+            do_sample=False,
+            eos_token_id=self.tokenizer.eos_token_id,
+            pad_token_id=self.tokenizer.eos_token_id,
+            max_length=self.max_length,
+            truncation=True,
+            stopping_criteria=stopping_criteria
+        )
+        # ans = response[0]["generated_text"]
+        ans = response[0]["generated_text"][len(prompt):]
+        return ans
+
+    def medrag_answer(self, question, options=None, k=32, rrf_k=100, save_dir = None, snippets=None, snippets_ids=None, **kwargs):
+
+        if options is not None:
+            options = '\n'.join([key+". "+options[key] for key in sorted(options.keys())])
+        else:
+            options = ''
+
+        # retrieve relevant snippets
+        if self.rag:
+            if snippets is not None:
+                retrieved_snippets = snippets[:k]
+                scores = []
+            elif snippets_ids is not None:
+                if self.docExt is None:
+                    self.docExt = DocExtracter(db_dir=self.db_dir, cache=True, corpus_name=self.corpus_name)
+                retrieved_snippets = self.docExt.extract(snippets_ids[:k])
+                scores = []
+            else:
+                assert self.retrieval_system is not None
+                retrieved_snippets, scores = self.retrieval_system.retrieve(question, k=k, rrf_k=rrf_k)
+
+            contexts = ["Document [{:d}] (Title: {:s}) {:s}".format(idx, retrieved_snippets[idx]["title"], retrieved_snippets[idx]["content"]) for idx in range(len(retrieved_snippets))]
+            if len(contexts) == 0:
+                contexts = [""]
+
+            contexts = [self.tokenizer.decode(self.tokenizer.encode("\n".join(contexts), add_special_tokens=False)[:self.context_length])]
+        else:
+            retrieved_snippets = []
+            scores = []
+            contexts = []
+
+        if save_dir is not None and not os.path.exists(save_dir):
+            os.makedirs(save_dir)
+
+        # generate answers
+        answers = []
+        if not self.rag:
+            prompt_cot = self.templates["cot_prompt"].render(question=question, options=options)
+            messages = [
+                {"role": "system", "content": self.templates["cot_system"]},
+                {"role": "user", "content": prompt_cot}
+            ]
+            ans = self.generate(messages)
+            answers.append(re.sub("\s+", " ", ans))
+        else:
+            for context in contexts:
+                prompt_medrag = self.templates["medrag_prompt"].render(context=context, question=question, options=options)
+                messages=[
+                        {"role": "system", "content": self.templates["medrag_system"]},
+                        {"role": "user", "content": prompt_medrag}
+                ]
+                ans = self.generate(messages)
+                answers.append(re.sub("\s+", " ", ans))
+        
+        if save_dir is not None:
+            with open(os.path.join(save_dir, "snippets.json"), 'w') as f:
+                json.dump(retrieved_snippets, f, indent=4)
+            with open(os.path.join(save_dir, "response.json"), 'w') as f:
+                json.dump(answers, f, indent=4)
+        
+        return answers[0] if len(answers)==1 else answers, retrieved_snippets, scores
+
+    def i_medrag_answer(self, question, options=None, k=32, rrf_k=100, save_path = None, n_rounds=4, n_queries=3, qa_cache_path=None, **kwargs):
+        if options is not None:
+            options = '\n'.join([key+". "+options[key] for key in sorted(options.keys())])
+        else:
+            options = ''
+        QUESTION_PROMPT = f"Here is the question:\n{question}\n\n{options}"
+
+        context = ""
+        qa_cache = []
+        if qa_cache_path is not None and os.path.exists(qa_cache_path):
+            qa_cache = eval(open(qa_cache_path, 'r').read())[:n_rounds]
+            if len(qa_cache) > 0:
+                context = qa_cache[-1]
+            n_rounds = n_rounds - len(qa_cache)
+        last_context = None
+
+        # Run in loop
+        max_iterations = n_rounds + 3
+        saved_messages = [{"role": "system", "content": self.templates["i_medrag_system"]}]
+
+        for i in range(max_iterations):
+            if i < n_rounds:
+                if context == "":
+                    messages = [
+                        {
+                            "role": "system",
+                            "content": self.templates["i_medrag_system"],
+                        },
+                        {
+                            "role": "user",
+                            "content": f"{QUESTION_PROMPT}\n\n{self.templates['follow_up_ask'].format(n_queries)}",
+                        },
+                    ]
+                else:                
+                    messages = [
+                        {
+                            "role": "system",
+                            "content": self.templates["i_medrag_system"],
+                        },
+                        {
+                            "role": "user",
+                            "content": f"{context}\n\n{QUESTION_PROMPT}\n\n{self.templates['follow_up_ask'].format(n_queries)}",
+                        },
+                    ]
+            elif context != last_context:
+                messages = [
+                    {
+                        "role": "system",
+                        "content": self.templates["i_medrag_system"],
+                    },
+                    {
+                        "role": "user",
+                        "content": f"{context}\n\n{QUESTION_PROMPT}\n\n{self.templates['follow_up_answer']}",
+                    },
+                ]
+            elif len(messages) == 1:
+                messages = [
+                    {
+                        "role": "system",
+                        "content": self.templates["i_medrag_system"],
+                    },
+                    {
+                        "role": "user",
+                        "content": f"{context}\n\n{QUESTION_PROMPT}\n\n{self.templates['follow_up_answer']}",
+                    },
+                ]
+            saved_messages.append(messages[-1])
+            if save_path:
+                with open(save_path, 'w') as f:
+                    json.dump([p if type(p) == dict else p.model_dump() for p in saved_messages], f, indent=4)
+            last_context = context
+            last_content = self.generate(messages)
+            response_message = {"role": "assistant", "content": last_content}
+            saved_messages.append(response_message)
+            if save_path:
+                with open(save_path, 'w') as f:
+                    json.dump([p if type(p) == dict else p.model_dump() for p in saved_messages], f, indent=4)       
+            if i >= n_rounds and ("## Answer" in last_content or "answer is" in last_content.lower()):
+                messages.append(response_message)
+                messages.append(
+                    {
+                        "role": "user",
+                        "content": "Output the answer in JSON: {'answer': your_answer (A/B/C/D)}" if options else "Output the answer in JSON: {'answer': your_answer}",
+                    }
+                )
+                saved_messages.append(messages[-1])
+                answer_content = self.generate(messages)
+                answer_message = {"role": "assistant", "content": answer_content}
+                messages.append(answer_message)
+                saved_messages.append(messages[-1])
+                if save_path:
+                    with open(save_path, 'w') as f:
+                        json.dump([p if type(p) == dict else p.model_dump() for p in saved_messages], f, indent=4)
+                return messages[-1]["content"], messages
+            elif "## Queries" in last_content:
+                messages = messages[:-1]
+                if last_content.split("## Queries")[-1].strip() == "":
+                    print("Empty queries. Continue with next iteration.")
+                    continue
+                try:
+                    action_str = self.generate([
+                        {
+                            "role": "user",
+                            "content": f"Parse the following passage and extract the queries as a list: {last_content}.\n\nPresent the queries as they are. DO NOT merge or break down queries. Output the list of queries in JSON format: {{\"output\": [\"query 1\", ..., \"query N\"]}}",
+                        },
+                    ])
+                    action_str = re.search(r"output\": (\[.*\])", action_str, re.DOTALL).group(1)
+                    action_list = [re.sub(r'^\d+\.\s*', '', s.strip()) for s in eval(action_str)]
+                except Exception as E:
+                    print("Error parsing action list. Continue with next iteration.")
+                    error_class = E.__class__.__name__
+                    error = f"{error_class}: {str(E)}"
+                    print(error)
+                    if save_path:
+                        with open(save_path + ".error", 'a') as f:
+                            f.write(f"{error}\n")                
+                    continue
+                for question in action_list:
+                    if question.strip() == "":
+                        continue
+                    try:
+                        rag_result = self.medrag_answer(question, k=k, rrf_k=rrf_k)[0]
+                        context += f"\n\nQuery: {question}\nAnswer: {rag_result}"
+                        context = context.strip()
+                    except Exception as E:
+                        error_class = E.__class__.__name__
+                        error = f"{error_class}: {str(E)}"
+                        print(error)
+                        if save_path:
+                            with open(save_path + ".error", 'a') as f:
+                                f.write(f"{error}\n")
+                qa_cache.append(context)
+                if qa_cache_path:
+                    with open(qa_cache_path, 'w') as f:
+                        json.dump(qa_cache, f, indent=4)
+            else:
+                messages.append(response_message)
+                print("No queries or answer. Continue with next iteration.")
+                continue
+        return messages[-1]["content"], messages
